495432c9ed38a74b89819fa453134405996c17e6
Zain Naqvi <zainnaqvi0@hotmail.com>
2023-12-11 22:14:50 +1100

    Your commit message here

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,15 @@
-console.log("what th");
+const words = [["Your mind", "Intellect", "Knowledge", "Wisdom"], ["expands", "grows", "develops", "blossoms"], ["like a flourishing garden.",
+"into a powerful force.",
+"with each passing day.",
+"making you unstoppable.",]]
+
+const message = () => {
+    const message = `${words[0][randomNumber()]} ${words[1][randomNumber()]} ${words[2][randomNumber()]}`
+    return message;
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+
+console.log(message());
